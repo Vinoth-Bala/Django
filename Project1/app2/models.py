35fcd2fc2d148cc9430cd5bd9f2d56f158a7b328
Vinoth-Bala <116018652+Vinoth-Bala@users.noreply.github.com>
2024-02-06 11:08:47 +0530

    Delete Project1 directory

--- Project1/app2/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Person(models.Model):
-    id = models.IntegerField(default=0),
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    ph_no = models.IntegerField(default=0)
-    Blood_Group = models.CharField(max_length=30)
